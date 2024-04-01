1aae2e216eb074fb7d6bc51878d62a6d0bf94aeb
AhmedThaer1 <110353919+AhmedThaer1@users.noreply.github.com>
2024-04-01 08:47:52 +0300

    Refactor MeetingPage component and add MeetingScreen, UpcomingMeetingScreen, and MeetingEndedScreen components

--- app/meeting/[id]/MeetingPage.tsx
+++ app/meeting/[id]/MeetingPage.tsx
@@ -1,54 +1,111 @@
 "use client";
 
+import { buttonClasses } from "@/components/Button";
+import useLoadCall from "@/hooks/useLoadCall";
+import useStreamCall from "@/hooks/useStreamCall";
 import { MeetingPageProps } from "@/interfaces";
+import { useUser } from "@clerk/nextjs";
 import {
-  Call,
   CallControls,
   SpeakerLayout,
   StreamCall,
   StreamTheme,
+  useCall,
+  useCallStateHooks,
   useStreamVideoClient,
 } from "@stream-io/video-react-sdk";
 import { Loader2 } from "lucide-react";
-import { useState } from "react";
+import Link from "next/link";
 
 const MeetingPage = ({ id }: MeetingPageProps) => {
-  const [call, setCall] = useState<Call>();
+  const { user, isLoaded: userLoaded } = useUser();
 
-  const client = useStreamVideoClient();
+  const { call, callLoading } = useLoadCall(id);
 
-  if (!client) {
+  if (!userLoaded || callLoading) {
     return <Loader2 className="mx-auto animate-spin" />;
   }
 
-  // const handleCall = async () => {
-  //   const call = client.call("default", id);
-  //   await call.join();
-  //   setCall(call);
-  // };
-
   if (!call) {
+    return <p className="text-center font-bold ">Call Not Found</p>;
+  }
+
+  const notAllowedToJoinCall =
+    call.type === "private-meeting" &&
+    (!user || !call.state.members.find((m) => m.user.id === user.id));
+
+  if (notAllowedToJoinCall) {
     return (
-      <button
-        onClick={async () => {
-          const call = client.call("privat-meeting", id);
-          await call.join();
-          setCall(call);
-        }}
-      >
-        Join Meeting
-      </button>
+      <p className="text-center font-bold">
+        You are not allowed to join this call. Please contact the host for
+        access.
+      </p>
     );
   }
 
   return (
     <StreamCall call={call}>
-      <StreamTheme className="space-y-3">
-        <SpeakerLayout />
-        <CallControls />
+      <StreamTheme>
+        <MeetingScreen />
       </StreamTheme>
     </StreamCall>
   );
 };
 
 export default MeetingPage;
+
+function MeetingScreen() {
+  const { useCallEndedAt, useCallStartsAt } = useCallStateHooks();
+
+  const callEndedAt = useCallEndedAt();
+  const callStartsAt = useCallStartsAt();
+
+  const callIsInFuture = callStartsAt && new Date(callStartsAt) > new Date();
+
+  const callHasEnded = !!callEndedAt;
+
+  if (callHasEnded) {
+    return <MeetingEndedScreen />;
+  }
+
+  if (callIsInFuture) {
+    return <UpcomingMeetingScreen />;
+  }
+
+  return <div>Call UI</div>;
+}
+
+function UpcomingMeetingScreen() {
+  const call = useStreamCall();
+
+  return (
+    <div className="flex flex-col items-center gap-6">
+      <p>
+        This Meeting Has Not Started Yet. it will start at{" "}
+        <span className="font-bold">
+          {call.state.startsAt?.toLocaleDateString()}
+        </span>
+      </p>
+      {call.state.custom.description && (
+        <p>
+          Description:{" "}
+          <span className="font-bold">{call.state.custom.description}</span>
+        </p>
+      )}
+      <Link className={buttonClasses} href="/">
+        Back Home
+      </Link>
+    </div>
+  );
+}
+
+function MeetingEndedScreen() {
+  return (
+    <div className="flex flex-col items-center gap-6">
+      <p className="font-bold">This Meeting Has Ended</p>
+      <Link className={buttonClasses} href="/">
+        Back Home
+      </Link>
+    </div>
+  );
+}
--- components/Button.tsx
+++ components/Button.tsx
@@ -5,15 +5,10 @@ const Button = ({
   className,
   ...props
 }: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
-  return (
-    <button
-      className={cn(
-        "flex items-center justify-center gap-2 rounded-full bg-blue-500 px-3 py-2 font-semibold text-white transition-colors hover:bg-blue-600 active:bg-blue-600 disabled:bg-gray-200",
-        className,
-      )}
-      {...props}
-    />
-  );
+  return <button className={cn(buttonClasses, className)} {...props} />;
 };
 
 export default Button;
+
+export const buttonClasses =
+  "flex items-center justify-center gap-2 rounded-full bg-blue-500 px-3 py-2 font-semibold text-white transition-colors hover:bg-blue-600 active:bg-blue-600 disabled:bg-gray-200";
